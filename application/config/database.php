54b9f65cbf59344395713b198f6662037a5ffd54
zia123123 <ziamuhamad@gmail.com>
2025-12-08 11:19:33 +0700

    add sql

--- application/config/database.php
+++ application/config/database.php
@@ -48,9 +48,9 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'zia123123';
+$db['default']['hostname'] = '127.0.0.1';
+$db['default']['username'] = 'bulogjabar';
+$db['default']['password'] = 'Bu7@2bfdw';
 $db['default']['database'] = 'spdjabar';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
